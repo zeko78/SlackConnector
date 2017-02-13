1d05471b26f8563fea5d06cde7d183a4f05f0490
Simon Colmer <simon_colmer@hotmail.com>
2017-02-13 20:02:05 +0000

    Created dummy app for @1iveowl

---
+++ src/ConsoleForDebugging/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlackConnector;
+
+namespace ConsoleForDebugging
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string slackKey = "ENTER SLACK KEY HER";
+
+            var connector = new SlackConnector.SlackConnector();
+
+            // timeouts here
+            var connection = connector.Connect(slackKey).Result;
+        }
+    }
+}
